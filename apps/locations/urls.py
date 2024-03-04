32378c1e8a9bc25bf3ebf29fc5788b85b9c9df56
Jamshid Boltaboyev <j.boltaboyev@uic.group>
2024-03-04 11:50:57 +0500

    feat: location module: get connector  single

--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from apps.locations.views import LocationListAPIView, LocationDetailAPIView
+from apps.locations.views import (
+    LocationListAPIView,
+    LocationDetailAPIView,
+    ChargePointDetailAPIView,
+    ConnectorDetailAPIView
+)
 
 urlpatterns = [
     path("", LocationListAPIView.as_view()),
     path("<str:location_id>/", LocationDetailAPIView.as_view()),
-    path("<str:location_id>/<str:evse_uid>/", LocationDetailAPIView.as_view()),
+    path("<str:location_id>/<str:evse_uid>/", ChargePointDetailAPIView.as_view()),
+    path("<str:location_id>/<str:evse_uid>/<str:connector_id>/", ConnectorDetailAPIView.as_view()),
 ]
--- apps/locations/views.py
+++ apps/locations/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from apps.locations.models import Location, ChargePoint
-from apps.locations.serializers import LocationSerializer
+from apps.locations.models import Location, ChargePoint, Connector
+from apps.locations.serializers import LocationSerializer, ChargePointSerializer, ConnectorSerializer
 
 
 class LocationListAPIView(APIView):
@@ -30,13 +30,29 @@ class LocationDetailAPIView(APIView):
     def get_object(self, location_id):
         return get_object_or_404(Location, id=location_id)
 
-class ChargePointDetail(APIView):
+
+class ChargePointDetailAPIView(APIView):
     def get(self, request, *args, **kwargs):
         instance = self.get_object(kwargs.get('location_id'), kwargs.get('evse_uid'))
 
-        serializer = LocationSerializer(instance)
+        serializer = ChargePointSerializer(instance)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def get_object(self, location_id, evse_uid):
         return get_object_or_404(ChargePoint, location_id=location_id, pk=evse_uid)
+
+
+class ConnectorDetailAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object(kwargs.get('location_id'), kwargs.get('evse_uid'), kwargs.get('connector_id'))
+
+        serializer = ConnectorSerializer(instance)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_object(self, location_id, evse_uid, connector_id):
+        return get_object_or_404(
+            Connector,
+            charge_point__location_id=location_id, charge_point_id=evse_uid, connector_id=connector_id
+        )
